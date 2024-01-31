3dce18febc8cba1a84c72a9721b0d2786ec1ba45
LQuesadaM <luisquesada163@gmail.com>
2024-01-31 09:57:59 -0300

    create function

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+
+
+// initialize buttons
+
+function goStore () {
+    console.log("Go to store.");
+}
+
+function goCave () {
+    console.log("Going to cave.");
+}
+
+function fightDragon() {
+    console.log("Fighting dragon.");
+}
