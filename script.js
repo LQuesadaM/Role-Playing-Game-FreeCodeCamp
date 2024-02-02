da2a37acd19d43bd1e865e988645be92b7ddfbba
LQuesadaM <luisquesada163@gmail.com>
2024-02-02 08:21:48 -0300

    creating a localization object to optimize

--- script.js
+++ script.js
@@ -19,11 +19,25 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const location = [
+    {
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"],
+        "button functions":  [goStore, goCave, fightDragon],
+        text: "You are in the town square. You see a sign that says \"Store\"."
+    }    
+];
+
 
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+
+function update(location) {
+    
+}
 
 function goTown() {
     button1.innerText = "Go to store";
