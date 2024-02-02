5b4ddf072709f8bfcffb1039ae8588a28b36bb15
LQuesadaM <luisquesada163@gmail.com>
2024-02-02 09:48:13 -0300

    logic of the function buyHealth

--- script.js
+++ script.js
@@ -74,18 +74,24 @@ function fightDragon () {
 }
 
 function buyHealth () {
-    
+    if (gold >= 10) {
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    } else {
+        text.innerText = "You do not have enough gold to buy health.";
+    }
 }
 
-/* function buyWeapon () {
+function buyWeapon () {
     
-} */
+} 
 
-/* function fightSlime () {
+function fightSlime () {
     
-} */
+} 
 
-/* function fightBeast () {
+function fightBeast () {
     
-} */
-
+} 
