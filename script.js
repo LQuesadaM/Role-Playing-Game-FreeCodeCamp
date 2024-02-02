ae18f2cf5d577b96309ad6e68d562d1717a6eeb9
LQuesadaM <luisquesada163@gmail.com>
2024-02-01 22:21:30 -0300

    create function goTown and goStore  property

--- script.js
+++ script.js
@@ -25,8 +25,24 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function goTown() {
+    button1.innerText = "Go to store";
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText = "You are in the town square. You see a sign that says Store.";
+}
+
 function goStore () {
-    console.log("Go to store.");
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You enter the store.";
 }
 
 function goCave () {
@@ -36,3 +52,14 @@ function goCave () {
 function fightDragon() {
     console.log("Fighting dragon.");
 }
+
+// function for the store
+
+function buyHealth() {
+    
+}
+
+function buyWeapon() {
+    
+}
+
