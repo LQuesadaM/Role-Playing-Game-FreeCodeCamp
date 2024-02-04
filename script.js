5297c23dc320871f9ae80070ecd153bc696cffac
LQuesadaM <luisquesada163@gmail.com>
2024-02-04 08:54:00 -0300

    add text for weapons in the inventory and weapon sell

--- script.js
+++ script.js
@@ -129,6 +129,9 @@ function sellWeapon () {
         goldText.innerText = gold;
         let currentWeapon = inventory.shift();
         text.innerText = `You sold a ${currentWeapon}.`
+        text.innerText += ` In your inventory you have: ${inventory}`
+    } else {
+        text.innerText = "Don't sell your only weapon!"
     }
 }
 
