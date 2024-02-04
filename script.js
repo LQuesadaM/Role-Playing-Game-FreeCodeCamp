59c72f23f86923d40a296b57d4e49aa0008da2ab
LQuesadaM <luisquesada163@gmail.com>
2024-02-04 08:05:19 -0300

    buyWeapons and push weapons to array inventory

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 const weapons = [
     {
         name: "stick",
-        power: "5"
+        power: 5
     },
     {
         name: "dagger",
@@ -104,7 +104,21 @@ function buyHealth () {
 }
 
 function buyWeapon () {
-    
+    if (currentWeapon < weapons.length - 1) {
+        if (gold >= 30) {
+            gold -= 30;
+            currentWeapon++;
+            goldText.innerText = gold;
+            let newWeapon = weapons[currentWeapon].name;
+            text.innerText = `You now have a ${newWeapon}.`;
+            inventory.push(newWeapon)
+            text.innerText += ` In your inventory you have: ${inventory}.`
+        } else {
+            text.innerText = "You do not have enough gold to buy a weapon.";
+        }
+    } else {
+        text.innerText = "You already have the most powerful weapon!";
+    }
 } 
 
 function fightSlime () {
