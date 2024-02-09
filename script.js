3973e4a7c32b4277e5c3f6230fe35dd41dd1b05c
LQuesadaM <luisquesada163@gmail.com>
2024-02-09 08:05:58 -0300

    create function isMonsterHit

--- script.js
+++ script.js
@@ -206,7 +206,13 @@ function attack () {
     text.innerText = `The ${monsters[fighting].name} attacks.`;
     text.innerText += ` You attack it with your ${weapons[currentWeapon].name}.`;
     health -= getMonsterAttackValue(monsters[fighting].level);
-    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+
+    if (isMonsterHit()) {
+        monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+    } else {
+        text.innerText += " You miss.";
+    }
+
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth; 
 
@@ -223,8 +229,11 @@ function attack () {
 
 function getMonsterAttackValue (level) {
     const hit = (level * 5) - (Math.floor(Math.random() * xp));
-    console.log(hit);
     return hit > 0 ? hit : 0;
+}
+
+function isMonsterHit () {
+    return Math.random() > .2 || health < 20;
 }
 
 function dodge () {
